2aac8d6237b4794e33f17af31763ff71ad4014d4
levibarbieri-bit <levibarbieri@gmail.com>
2021-05-31 20:45:22 -0300

    Adicioana complementação automatica de cep

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let show = true; 
+let show = true;
 
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
@@ -13,6 +13,86 @@ menuToggle.addEventListener("click", () => {
 
 
 function backToTop() {
- 
+
     window.scrollTo(0, 0);
+}
+
+function limpa_formulario_cep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('endereco').value = ("");
+    document.getElementById('bairro').value = ("");
+    document.getElementById('cidade').value = ("");
+    document.getElementById('estado').value = ("");
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('endereco').value = (conteudo.logradouro);
+        document.getElementById('bairro').value = (conteudo.bairro);
+        document.getElementById('cidade').value = (conteudo.localidade);
+        document.getElementById('estado').value = (conteudo.uf);
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpa_formulario_cep();
+        alert("CEP não encontrado.");
+    }
+}
+
+function getHttpRequest() {
+    if (window.XMLHttpRequest) {
+        req = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        req = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    return req;
+}
+
+function pesquisacep() {
+
+    var cep = document.getElementById('cep').value
+    var cep = cep.replace(/\D/g, '');
+
+    if (cep != "") {
+
+        var validacep = /^[0-9]{8}$/;
+
+        if (validacep.test(cep)) {
+            document.getElementById('endereco').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            document.getElementById('estado').value = "...";
+
+            var req = getHttpRequest();
+
+            var url = "https://viacep.com.br/ws/" + cep + "/json/";
+            req.onreadystatechange = function () {
+                if (req.readyState == 4) {
+                    if (req.status == 200) {
+
+                        var retorno = JSON.parse(req.responseText);
+                        meu_callback(retorno)
+                    }
+                }
+            }
+            req.open("GET", url, true);
+            req.send(null);
+        }
+        else {
+            limpa_formulario_cep();
+            alert("Formato de CEP inválido.");
+        }
+    } else {
+        limpa_formulario_cep();
+    }
+};
+
+function mascara(t, mask) {
+    var i = t.value.length;
+    var saida = mask.substring(1, 0);
+    var texto = mask.substring(i)
+    if (texto.substring(0, 1) != saida) {
+        t.value += texto.substring(0, 1);
+    }
 }
